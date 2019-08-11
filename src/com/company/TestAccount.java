788285104d5a3fc4d2ce6cfb87603b7a87191334
Lucas Munoz <lucas00792@gmail.com>
2019-08-11 16:43:21 -0500

    simplified some returns and added comments

--- src/com/company/TestAccount.java
+++ src/com/company/TestAccount.java
@@ -1,22 +1,22 @@
 package com.company;
 
 public class TestAccount {
-    String bankAccount;
-    String accountNumber;
-    String routingNumber;
+    private String bankAccount;
+    private String accountNumber;
+    private String routingNumber;
+
     TestAccount(String bankAccount){
         this.bankAccount = bankAccount.replaceAll("\\s", "").replace("-", "");
         this.routingNumber = this.bankAccount.substring(0, 9);
         this.accountNumber = this.bankAccount.substring(9);
     }
 
+    // To check and make sure the routing and account numbers have been validated
     public boolean isValid() {
-        if (isRoutingNumberValid() && isUserAccountValid()) {
-            return true;
-        }
-        return false;
+        return (isRoutingNumberValid() && isUserAccountValid());
     }
 
+    // Runs a check on the routing number to make sure we will have no remainder from the calculated sum
     public boolean isRoutingNumberValid() {
             int checkSumTotal = 0;
 
@@ -30,19 +30,15 @@ public class TestAccount {
                 checkSumTotal += Character.getNumericValue(routingNumber.charAt(i + 2));
             }
 
-            if ((checkSumTotal % 10) != 0){
-                return false;
-            }
-            return true;
+        return ((checkSumTotal % 10) == 0);
     }
 
+    // validates that the users account is at least 5 characters long
     public boolean isUserAccountValid() {
-        if (accountNumber.length() >= 5){
-            return true;
-        }
-        return false;
+        return (accountNumber.length() >= 5);
     }
 
+    // returns a snip of the bank account number such as 123271978...889
     public String sanitized() {
         return routingNumber + "..." + bankAccount.substring(bankAccount.length() - 3);
     }
